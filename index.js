30a2b960559b2a10cdf3e44474e0f8d538445697
Marton Tasnadi <marton.tasnadi@gmail.com>
2017-09-02 23:34:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function getHungarianCountySeat(county) {
+	const counties = {
+		'Bács-Kiskun': 'Kecskemét',
+		'Baranya': 'Pécs',
+		'Békés': 'Békéscsaba',
+		'Borsod-Abaúj-Zemplén': 'Miskolc',
+		'Csongrád': 'Szeged',
+		'Fejér': 'Székesfehérvár',
+		'Győr-Moson-Sopron': 'Győr',
+		'Hajdú-Bihar': 'Debrecen',
+		'Heves': 'Eger',
+		'Jász-Nagykun-Szolnok': 'Szolnok',
+		'Komárom-Esztergom': 'Tatabánya',
+		'Nógrád': 'Salgótarján',
+		'Pest': 'Budapest',
+		'Szabolcs-Szatmár-Bereg': 'Nyíregyháza',
+		'Szabolcs-Szatmár-Bereg': 'Nyíregyháza',
+		'Tolna': 'Szekszárd',
+		'Vas': 'Szombathely',
+		'Veszprém': 'Veszprém',
+		'Zala': 'Zalaegerszeg'
+	}
+
+	return counties[county]
+}
+
+module.exports.hungarian_contyseats = getHungarianCountySeat
